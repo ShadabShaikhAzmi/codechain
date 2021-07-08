eecef3470118406dcaaf23ea66ea8e78be237bb0
shadab <shadabshaikhazmi72@gmail.com>
2021-07-08 16:26:12 +0530

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e17367fc8e832d19e4fd3bf5c02b1cdf29895197',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e17367fc8e832d19e4fd3bf5c02b1cdf29895197',
+    ),
+  ),
+);
